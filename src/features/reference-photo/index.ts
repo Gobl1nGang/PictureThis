bbdcaf5fb415d19cb47541d737773d4f1fef117d
R0yZh3ng <royyi2006@gmail.com>
2025-11-22 13:28:41 -0800

    changed analysis to be on click ratehr than real time, fixed prompting to be precise

--- src/features/reference-photo/index.ts
+++ src/features/reference-photo/index.ts
@@ -1,16 +1,13 @@
 // Export all reference photo feature components, hooks, and services
 export { SetReferenceButton } from './components/SetReferenceButton';
-export { ReferenceAnalysisModal } from './components/ReferenceAnalysisModal';
-export { ReferenceModeIndicator } from './components/ReferenceModeIndicator';
-export { LiveCoachingOverlay } from './components/LiveCoachingOverlay';
 export { AnalysisModal } from './components/AnalysisModal';
 
 export { useReferencePhoto } from './hooks/useReferencePhoto';
 
-export { 
-  analyzeReferencePhoto, 
-  calculateMatchScore, 
-  generateCoachingTips 
+export {
+  analyzeReferencePhoto,
+  calculateMatchScore,
+  generateCoachingTips
 } from './services/analysisService';
 
 export * from './types';
